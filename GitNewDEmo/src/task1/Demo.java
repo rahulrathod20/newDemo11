0d9d40c6665b5dbeb363966d78a7e9c3948668e7
rahulrathod20 <104188895+rahulrathod20@users.noreply.github.com>
2022-04-26 12:46:08 +0530

    NewBranch

--- GitNewDEmo/src/task1/Demo.java
+++ GitNewDEmo/src/task1/Demo.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo 
 
-{       public void m1()
-      {     
-	System.out.println("hi");
-       }
+{     
         public void m2()
       {     
 	System.out.println("hi");
